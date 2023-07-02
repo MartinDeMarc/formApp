3575493c4d7773e0215cddc84ad1023888e7a358
Martin Enriquez <martin.enriquez.seb@gmail.com>
2023-07-01 21:48:21 -0300

    validation page basic

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+title: string;
+route: string;
+}
+
+
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'shared-side-menu',
   templateUrl: './side-menu.component.html',
   styles: [
   ]
 })
 export class SideMenuComponent {
+
+public reactiveMenu: MenuItem[] = [
+  {title: 'Basicos', route: './reactive/basic'},
+  {title: 'Dynamic', route: './reactive/dynamic'},
+  {title: 'Switches', route: './reactive/switches'}
+]
+public authMenu: MenuItem[] = [
+  {title: 'Registro', route: './auth'},
+]
 
 }
